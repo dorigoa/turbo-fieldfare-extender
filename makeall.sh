860c3340e74369d4f6d58843d0f4f3c0b8977938
dorigoa <alvise72@live.com>
2026-08-07 22:42:14 +0200

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,6 @@
 git clone git@github.com:dorigoa/turbo-fieldfare-fork.git
-python3 ./apply_gemma_qat.py --repo-path ./turbo-fieldfare/
-cd turbo-fieldfare
+python3 ./apply_gemma_qat.py --repo-path ./turbo-fieldfare-fork/
+cd turbo-fieldfare-fork
 mkdir -p Scratch
 cp ../build-app.sh Scratch/
 source Scratch/build-app.sh --install
