9df42404682da3549586878421b549f2354c0d44
dorigoa <alvise72@live.com>
2026-08-07 22:34:38 +0200

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/drumih/turbo-fieldfare
+git clone https://github.com/dorigoa/turbo-fieldfare
 python3 ./apply_gemma_qat.py --repo-path ./turbo-fieldfare/
 cd turbo-fieldfare
 mkdir -p Scratch
